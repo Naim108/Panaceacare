d8a195141b1bf5cc32f12b4fec32459d8e58be8a
Naim Hossain <nayeemhossain108@gmail.com>
2023-11-30 01:30:54 +0600

    carosel added

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,4 +20,20 @@ $(window).on('load',function(){
     }
     return false;
   });
- })
+  var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:2,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+ });
+ 
